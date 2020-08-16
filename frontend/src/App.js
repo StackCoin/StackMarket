125d57ad24ddb62c5c18dc12c981594061f8b831
Mitchell Hynes <mshynes@mun.ca>
2020-08-16 08:34:50 -0230

    Frontend change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,27 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import gql from "graphql-tag";
+import logo from './logo.svg';
+import './App.css';
 
-const GET_POKEMON_INFO = gql`
-{
-    user(first: 150) {
-      id
-      number
-      name,
-      image,
-      evolutions {
-        id,
-        number,
-        name,
-        image
-      }
-    }
-  }
-`;
-
-export default function App() {
-  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-
+function App() {
+  console.log(process.env.REACT_APP_API_URL)
   return (
-  <React.Fragment>
-    <h1>Pokémons</h1>
-
-    <p>
-      <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">
-        The Pokémon franchise
-      </a>{" "}
-      revolves around 832 fictional species of collectible monsters, each having
-      unique designs and skills. Conceived by Satoshi Tajiri in early 1989,
-      Pokémon are creatures that inhabit the fictional Pokémon World. This is
-      the list of the first 150 Pokémon as they appear in Pokémon Stadium,
-      starting with Bulbasaur in the top left corner and ending with Mewtwo in
-      the bottom right corner.
-    </p>
-    <div className="container">
-      {data &&
-        data.pokemons &&
-        data.pokemons.map((pokemon, index) => (
-          <div key={index} className="card">
-            <img src={pokemon.image} />
-            <div class="card-body">
-              <h3>{pokemon.name}</h3>
-              <p>
-                {pokemon.evolutions && pokemon.evolutions.length !== 0 && (
-                  <p>
-                    {" "}
-                    Evolutions:
-                    {pokemon.evolutions.map((e, indx) => {
-                      return <p key={indx}> {e.name} </p>;
-                    })}
-                  </p>
-                )}
-              </p>
-            </div>
-          </div>
-        ))}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </React.Fragment>
   );
-};
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,22 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: process.env.FRONTEND_API_URL
-})
-
-const client = new ApolloClient({
-  cache,
-  link
-})
-
-ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
