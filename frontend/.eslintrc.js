8afab1858aef395b5ce62f48c3e49453292aefe3
Mitchell Hynes <mshynes@mun.ca>
2020-08-19 00:13:32 -0230

    Prettier, eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 10,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { gql } from '@apollo/client';
-import { Auth0Provider } from "@auth0/auth0-react";
-import { useAuth0 } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
 
@@ -17,11 +14,18 @@ function Routing() {
   const { isLoading, error } = useAuth0();
 
   if (error) {
-    return <div>FUCK@!!! Ireally screwed up man, and I'm sorry... Please please please forgive me.... IM SORRY I said I'm sorry man!!!!<br/>{error.message}</div>
+    return (
+      <div>
+        FUCK@!!! Ireally screwed up man, and Im sorry... Please please please
+        forgive me.... IM SORRY I said Im sorry man!!!!
+        <br />
+        {error.message}
+      </div>
+    );
   }
 
   if (isLoading) {
-    return "Loading..."
+    return 'Loading...';
   }
 
   return (
@@ -30,16 +34,16 @@ function Routing() {
         <Route exact path="/">
           <div className="App">
             <header className="App-header">
-              <LoginButton/>
+              <LoginButton />
             </header>
           </div>
         </Route>
         <Route exact path="/dashboard">
-          <Dashboard/>
+          <Dashboard />
         </Route>
       </Switch>
     </Router>
-  )
+  );
 }
 
 function App() {
@@ -48,10 +52,9 @@ function App() {
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={`${process.env.REACT_APP_URL}/dashboard`}
-      onRedirectCallback={() => console.log("Hello world")}
       audience={process.env.REACT_APP_AUTH0_AUDIENCE}
     >
-      <Routing/>
+      <Routing />
     </Auth0Provider>
   );
 }
