af8decda1fd397179fa60b2802553260cf526904
Mitchell Hynes <mshynes@mun.ca>
2020-12-28 23:49:11 -0330

    Redef window env for both prod and dev

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+window.__env__ = Object.fromEntries(
+  Object.entries({
+    ...window.__env__,
+  }).filter(([_, value]) => value !== '<no value>')
+);
+
 if (process.env.NODE_ENV === 'development') {
-  window.__env__ = Object.fromEntries(
-    Object.entries({
-      ...window.__env__,
-      ...process.env,
-    }).filter(([_, value]) => value !== '<no value>')
-  );
+  window.__env__ = {
+    ...window.__env__,
+    ...process.env,
+  };
 }
 
 ReactDOM.render(
